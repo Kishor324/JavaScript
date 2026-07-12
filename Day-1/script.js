a35d3cc06c69b46270c80e97bdf463db8438783f
Kishor Bhatt <https://github.com/Kishor324/JavaScript.git>
2026-07-12 10:15:12 +0545

    Introducton of js

---
+++ Day-1/script.js
@@ -0,0 +1,37 @@
+//Variables and Declarations in JavaScript
+
+//A variable is a named container used to store data. Before using a variable, you usually declare it.
+
+//let
+let name = "Kishor";
+console.log(name);
+
+name = "Ram";
+console.log(name);
+
+//
+const
+
+const name = hari;
+console.log(PI);
+
+
+
+//var
+var age = 20;
+console.log(age);
+
+var age = 21;
+console.log(age);
+
+
+//declarations
+
+if (true) {
+    var a= 1;
+    let b= 2;
+
+
+}
+ console.log(a); //1
+ console.log(b); // ReferenceError: b is not defined
